79e28670e78ccf8f74a52e90acb561f6b0329ff1
AmitMaymon <145137599+AmitMaymon@users.noreply.github.com>
2024-07-15 23:19:45 +0300

    Update config.js

--- database/config.js
+++ database/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect("") // enter your mongodb cluster login
+mongoose.connect("mongodb+srv://chatAppTester:A123456@cluster0.ajdwy4s.mongodb.net/chatApp?retryWrites=true&w=majority&appName=Cluster0") // enter your mongodb cluster login OR use this restricted one
     .then(data => { console.log('Connected to DB'); })
     .catch(e => console.log(e))
